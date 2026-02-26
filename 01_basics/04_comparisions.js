b41d06fd4b9442bab61a296d64105d54ab3716a0
Sharanyaa G <sharanyaag30@gmail.com>
2026-02-26 14:44:03 +0000

    comparision

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+/// === strict equality operator. It compares two values for equality by checking both the value and the data type without performing any type conversion (coercion).
+
+console.log("2" === 2);
+
