f5309ea18d10596636c5cd9b8a53a8fa1d970e32
Sharanyaa G <sharanyaag30@gmail.com>
2026-02-26 14:19:01 +0000

    conversions

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -41,5 +41,46 @@ let stringNumber = String(someNumber)
 
 let value = 3
 let negvalue = -value
-console.log(negvalue);
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = "world"
+
+let str3 = str1 + " " + str2      
+// console.log(str3);   
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + ("2"));
+
+
+// console.log((3 + 4 )* 5 % 3); bodmas
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+console.log(num1);
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
 
