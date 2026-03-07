9ec24edf52bca35041872d6bb3cc17d98db9b413
Sharanyaa G <sharanyaag30@gmail.com>
2026-03-07 08:38:29 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,51 @@
+// const name = "sharanyaa"
+// const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+
+// console.log(`Hello my name is  ${name.toUpperCase()} and my repo count is ${repoCount}`);
+
+// const gameName = new String("chess")
+// console.log(gameName);
+// console.log(gameName[2]);
+// console.log(gameName.__proto__);
+
+
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// console.log(name.charAt(2));
+// console.log(name.indexOf("a"));
+// console.log(name.lastIndexOf("a"));
+// console.log(name.includes("shar"));
+// console.log(name.startsWith("sha"));
+// console.log(name.endsWith("yaa"));
+// console.log(name.substring(0, 4));
+// console.log(name.slice(0, 4));
+// console.log(name.split("a"));
+// console.log(name.replace("shar", "sneha"));
+
+
+// const newString = name.substring(0,4)
+// console.log(newString);
+
+// const nextString = name.slice(-8,4)
+// console.log(nextString);
+
+// const nextonestring = "   sharanyaa    "
+// console.log(nextonestring);
+// console.log(nextonestring.trim());
+
+
+// const url = "https://www.youtube.com/channel/UCW5YeuERMmlnqo4oq8vwUpg"  
+
+
+// console.log(url.replace("https://www.youtube.com/channel/", ""));
+
+// console.log(url.includes("youtube"));
+
+
+newString = "sharanyaa"
+
+console.log(newString.split('a'));
