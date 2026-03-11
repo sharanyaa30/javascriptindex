9fefb41f220a34ae45a758041367aca1c0b5ca0a
Sharanyaa G <sharanyaag30@gmail.com>
2026-03-11 16:30:28 +0000

    calling function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -13,9 +13,28 @@ function sayMyName(){
 
 // sayMyName(); // function call
 
+// function addTwoNumbers(num1, num2){ // parameters
+//     console.log(num1 + num2);
+// }
+
 function addTwoNumbers(num1, num2){ // parameters
-    console.log(num1 + num2);
+//    let result = num1 + num2;
+//    return result;
+
+    return num1 + num2; // directly returning the result without storing in a variable  
+}
+const result =addTwoNumbers(3, 5); // function call with arguments
+
+// console.log("result:", result);
+
+
+function loginUserMessage(username = "Guest"){ // default parameter value
+    if(!username){
+        return "Please enter a username"
+    }
+    return `${username} just logged in`
 }
 
-addTwoNumbers(3, "a"); // function call with arguments
+console.log(loginUserMessage()); // function call with argument
 
+// if values are not passed it will be undefined
