5a7f78908b011358002bc936c4ee3ab91acc0221
Sharanyaa G <sharanyaag30@gmail.com>
2026-03-11 16:06:44 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,21 @@
+function sayMyName(){
+    console.log("S");
+    console.log("h");
+    console.log("a");
+    console.log("r");
+    console.log("a");
+    console.log("n");
+    console.log("y");
+    console.log("a");
+    console.log("a");
+}
+
+
+// sayMyName(); // function call
+
+function addTwoNumbers(num1, num2){ // parameters
+    console.log(num1 + num2);
+}
+
+addTwoNumbers(3, "a"); // function call with arguments
+
